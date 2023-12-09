76e64075e807ab2e5a8196da6a2e65429667fde6
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2023-12-09 21:26:10 +0200

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,25 +1,27 @@
-def split_and_store_from_file(file_path):
+def convert_data(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-
-    seed_line = next((line for line in lines if line.startswith('seeds:')), None)
-    if seed_line:
-        seed_list = list(map(int, seed_line.split(':')[1].strip().split()))
-    else:
-        seed_list = []
-
-    map_lines = [line.strip() for line in lines if not line.startswith('seeds:')]
-    data = [line.split(':') for line in map_lines]
+    seeds, *map_lines = lines
     maps = {}
-    key = ""
-    for map_data in data[1:]:
-        if len(map_data[0]) > 2 and map_data[0].split()[0].isdigit():
+    for map_data in [line.split(':') for line in map_lines]:
+        if len(map_data[0]) > 2 and map_data[0][0].isdigit():
             maps[key].append(tuple(map(int, map_data[0].split())))
         elif len(map_data[0].split()) > 0 and not map_data[0].split()[0].isdigit():
-            maps[map_data[0].strip()] = []
-            key = map_data[0].strip()
+            key, maps[key] = map_data[0].strip(), []
 
-    return seed_list, maps
+    return list(map(int, seeds.split()[1:])), maps
+
+
+def find_lowest_location(seeds, maps):
+    lowest_location = float('inf')
+    conversion_order = [key for key in maps.keys() if key.endswith('map')]
+
+    for seed in seeds:
+        location = seed
+        for category in conversion_order:
+            location = convert_category(location, maps[category])
+        lowest_location = min(lowest_location, location)
+    return lowest_location
 
 
 def convert_category(seed, maps):
@@ -31,27 +33,5 @@ def convert_category(seed, maps):
     return seed
 
 
-def find_lowest_location(seeds, maps):
-    lowest_location = float('inf')
-
-    conversion_order = [
-        'seed-to-soil map',
-        'soil-to-fertilizer map',
-        'fertilizer-to-water map',
-        'water-to-light map',
-        'light-to-temperature map',
-        'temperature-to-humidity map',
-        'humidity-to-location map'
-    ]
-
-    for seed in seeds:
-        location = seed
-        for category in conversion_order:
-            location = convert_category(location, maps[category])
-        lowest_location = min(lowest_location, location)
-    return lowest_location
-
-
-seeds, maps = split_and_store_from_file("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input5.txt")
-
+seeds, maps = convert_data("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input5.txt")
 print("part1: Lowest Location Number:", find_lowest_location(seeds, maps))

9bb1c4a08a2183c4b3f199f371a967791b7de2cd
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2023-12-11 14:09:48 +0200

    Update day11.py
    shortened code a bit

--- day11.py
+++ day11.py
@@ -1,49 +1,28 @@
 import numpy as np
+import itertools
 
 
 def expand(grid):
-    rows, cols = grid.shape
-    empty_rows = []
-    empty_cols = []
-
-    for i in range(rows):
-        if all(c == '.' for c in grid[i]):
-            empty_rows.append(i)
-
-    for j in range(cols):
-        if all(c == '.' for c in grid[:, j]):
-            empty_cols.append(j)
-
-    return empty_rows, empty_cols
+    return tuple(map(lambda axis: [i for i in range(grid.shape[axis]) if
+                                    all(c == '.' for c in (grid[i] if axis == 0 else grid[:, i]))], [0, 1]))
 
 
 def find_points(grid):
-    rows, cols = grid.shape
-    points = []
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == '#':
-                points.append((i, j))
-    return points
+    return [(i, j) for i in range(grid.shape[0]) for j in range(grid.shape[1]) if grid[i][j] == '#']
+
+
+def calc_man_dist(x, y, expand, num):
+    return abs(x - y) + sum(num - 1 for c in expand if min(x, y) <= c <= max(x, y))
 
 
 def cal_min_dis(points, expand_rows, expand_cols, num):
-    total = 0
-    for r1, c1 in points:
-        for r2, c2 in points:
-            c_sum = abs(c2 - c1) + sum(num-1 for c in expand_cols if min(c2, c1) <= c <= max(c2, c1))
-            r_sum = abs(r2 - r1) + sum(num-1 for r in expand_rows if min(r2, r1) <= r <= max(r2, r1))
-            total += c_sum + r_sum
-    return int(total / 2)
+    return int(sum((calc_man_dist(r2, r1, expand_rows, num) + calc_man_dist(c2, c1, expand_cols, num))
+                   for (r1, c1), (r2, c2) in itertools.combinations(points, 2)))
 
 
-with open("input11.txt", "r") as file:
+with open("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input11.txt", "r") as file:
     grid = np.array([list(line.strip()) for line in file])
 rows, cols = expand(grid)
 
-print(cal_min_dis(find_points(grid), rows, cols, 2))
-print(cal_min_dis(find_points(grid), rows, cols, 1000000))
-
-
-
-
+print('part 1 solution: ', cal_min_dis(find_points(grid), rows, cols, 2), 
+      '\npart 2 solution:', cal_min_dis(find_points(grid), rows, cols, 1000000))

727600e3385be94ad0c0ff25c501645d281ac8b4
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2023-12-16 22:43:49 +0200

    Update day16.py
    shortened the code

--- day16.py
+++ day16.py
@@ -1,66 +1,37 @@
 from collections import deque
 import numpy as np
-file = open("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input16.txt", "r").readlines()
-grid = [list(line.strip()) for line in file]
+file = [line.strip() for line in open("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input16.txt", "r").readlines()]
+directionsR = {
+    '.': lambda r, dirR, c, dirC: [(r + dirR, c, dirR, 0)], '|': lambda r, dirR, c, dirC: [(r + dirR, c, dirR, 0)],
+    '/': lambda r, dirR, c, dirC: [(r + dirR, c, 0, -dirR)], '\\': lambda r, dirR, c, dirC: [(r + dirR, c, 0, dirR)],
+    '-': lambda r, dirR, c, dirC: [(r + dirR, c, 0, -1), (r + dirR, c, 0, 1)]
+}
+directionsC = {
+    '.': lambda r, dirR, c, dirC: [(r, c + dirC, 0, dirC)], '-': lambda r, dirR, c, dirC: [(r, c + dirC, 0, dirC)],
+    '/': lambda r, dirR, c, dirC: [(r, c + dirC, -dirC, 0)], '\\': lambda r, dirR, c, dirC: [(r, c + dirC, dirC, 0)],
+    '|': lambda r, dirR, c, dirC: [(r, c + dirC, 1, 0), (r, c + dirC, -1, 0)]
+}
 
 
 def beam(r, c, dirR, dirC):
-    queue = deque([(r, c, dirR, dirC)])
-    visited = set()
-
+    queue, visited = deque([(r, c, dirR, dirC)]), set()
     while queue:
         r, c, dirR, dirC = queue.popleft()
         if (r, c, dirR, dirC) in visited:
             continue
         visited.add((r, c, dirR, dirC))
-        charged[r][c] = 1  # Marking the cell
-
-        if dirR != 0 and -1 < r + dirR < len(grid):
-            directions = {
-                '.': [(r+dirR, c, dirR, 0)],
-                '|': [(r+dirR, c, dirR, 0)],
-                '/': [(r+dirR, c, 0, -dirR)],
-                '\\': [(r+dirR, c, 0, dirR)],
-                '-': [(r + dirR, c, 0, -1), (r + dirR, c, 0, 1)]
-            }
-            # traveling up or down
-            if grid[r + dirR][c] in directions:
-                for row, col, dr, dc in directions[grid[r+dirR][c]]:
-                    queue.append((row, col, dr, dc))
-
-        elif -1 < c + dirC < len(grid[0]):
-            # traveling right or left
-            directions = {
-                '.': [(r, c + dirC, 0, dirC)],
-                '-': [(r, c + dirC, 0, dirC)],
-                '/': [(r, c + dirC, -dirC, 0)],
-                '\\': [(r, c + dirC, dirC, 0)],
-                '|': [(r, c + dirC, 1, 0), (r, c + dirC, -1, 0)]
-            }
-            if grid[r][c + dirC] in directions:
-                for row, col, dr, dc in directions[grid[r][c + dirC]]:
-                    queue.append((row, col, dr, dc))
-
+        charged[r][c] = 1
+        if not -1 < r + dirR < len(grid) or not -1 < c + dirC < len(grid[0]):
+            continue
+        dirs, key = directionsR if dirR != 0 else directionsC, grid[r + dirR][c] if dirR != 0 else grid[r][c + dirC]
+        queue.extend((row, col, dr, dc) for row, col, dr, dc in dirs.get(key, [])(r, dirR, c, dirC))
 
 
 scores = []
-for i in range(len(grid)):
-    for d in [1, -1]:
+for i in range(len(file)):
+    for d, y in [(1, 0), (-1, len(file)-1)]:
         grid = [list(line.strip()) for line in file]
         charged = np.array([list(0 for _ in line) for line in grid])
-        dirX, dirY = d, 0
-        if grid[i][0] == '\\':
-            dirY, dirX = d, 0
-        elif grid[i][0] == '/':
-            dirY, dirX = -d, 0
-        elif grid[i][0] == '-':
-            beam(i, 0, 0, -1)
-            dirY, dirX = d, 0
-        beam(i, 0 if d == 1 else len(grid)-1, dirY, dirX)
-
+        beam(i, y, *{'\\': (d, 0), '/': (0, -d)}.get(grid[i][y], (0, d)))
         scores.append(np.sum(charged))
-
-
-print('part 1 solution:', scores[0])
-print('part 2 solution:', max(scores))
-
+print('part 1 solution:', scores[0], '\npart 2 solution:', max(scores))

0b493c7cce494b5dbd70431ffea6b20f211bc571
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2023-12-18 17:33:45 +0200

    Update day18.py

--- day18.py
+++ day18.py
@@ -3,10 +3,8 @@ puzzle = [line.strip().split() for line in open("C:\\Users\\USER\\PycharmProject
 dirs = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (-1, 0)}
 lagoon, lagoon2, end, end2 = [], [], (0, 0), (0, 0)
 for angle, length, color in puzzle:
-    end = (end[0] + int(length) * dirs[angle][0], end[1] + int(length) * dirs[angle][1])
-    lagoon.append(end)
-    length = int(color[2:7], 16)
+    lagoon.append(end := (end[0] + int(length) * dirs[angle][0], end[1] + int(length) * dirs[angle][1]))
     x, y = list(dirs.values())[int(color[7])]
-    end2 = (end2[0] + length * x, end2[1] + length * y)
-    lagoon2.append(end2)
+    side = int(color[2:7], 16)
+    lagoon2.append(end2 := (end2[0] + side * x, end2[1] + side * y))
 print([('part'+str(i+1), (lambda p: int(p.area+p.length//2+1))(Polygon(d))) for i, d in enumerate([lagoon, lagoon2])])

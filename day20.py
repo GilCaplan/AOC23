08333d2fc5ee38931840b2c364025af028146225
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2023-12-21 00:05:20 +0200

    Update day20.py
    entire solution, part 1 & part 2
    
    for part 2 I make the assumption that I find the cycles for the four conjunctions in less than 4000 cycles (button pressed)

--- day20.py
+++ day20.py
@@ -4,13 +4,17 @@ execute = deque([])
 low_pulse, high_pulse = 0, 0
 part2 = {'ng': 1, 'sv': 1, 'ft': 1, 'jz': 1}
 cjj = list(part2.keys())
-cycle = 0
+
+with open("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input20.txt", 'r') as file:
+    input1 = [line.strip().split(' -> ') for line in file]
+
+
 # ng, sv, ft, jz
 # how many cycles does it take for each one to get all high
 # (%): if high_pulse nothing, else low_pulse then switch from on to off or off to on
 # if was off then send high pulse, if was on then send low_pulse
-def flipflop(key, pulse):
-    global high_pulse, low_pulse, modules, cycle
+def flipflop(key, pulse, modules, cycle):
+    global high_pulse, low_pulse
     if pulse == 'low':
         modules[key] = (modules[key][0], pl := [1] if modules[key][1] == [0] else [0])
         for kl in modules[key][0]:
@@ -22,19 +26,22 @@ def flipflop(key, pulse):
 
 # (&): remembers the most recent pulse type from each connected module, default low_pulse
 # if all modules pulses are high then send low_pulse else high_pulse
-def conjunction(key, pulse, prev_key):
-    global low_pulse, high_pulse, cycle
+def conjunction(key, pulse, prev_key, modules, cycle):
+    global low_pulse, high_pulse
     if prev_key is not None:
         modules[key][1][prev_key] = 0 if pulse == 'low' else 1
     pl = 'low' if sum(modules[key][1].values()) == len(list(modules[key][1].keys())) else 'high'
+    if pl == 'high' and key[1:] in list(part2.keys()):
+        part2[key[1:]] = cycle
     for kl in modules[key][0]:
         high_pulse += 1 if pl == 'high' else 0
         low_pulse += 1 if pl != 'high' else 0
         kl = kl.strip()
         execute.append((kl, pl, key))
 
-def send_pulses(key, pulse, prev):
-    global low_pulse, high_pulse, execute, modules, cycle
+
+def send_pulses(key, pulse, prev, modules, cycle):
+    global low_pulse, high_pulse, execute
     if key == 'broadcaster':
         for module in modules[key][0]:
             module = module.strip()
@@ -43,39 +50,35 @@ def send_pulses(key, pulse, prev):
     else:
         if (key if '%' in key else '%'+key) in modules.keys():
             key = key if '%' in key else '%' + key
-            flipflop(key, pulse)
+            flipflop(key, pulse, modules, cycle)
         elif (key if '&' in key else '&'+key) in modules.keys():
             key = key if '&' in key else '&' + key
-            conjunction(key, pulse, prev)
+            conjunction(key, pulse, prev, modules, cycle)
 
 
-with open("C:\\Users\\USER\\PycharmProjects\\AOC2023\\input20.txt", 'r') as file:
-    input1 = [line.strip().split(' -> ') for line in file]
-modules = {line[0].strip(): (line[1].split(','), [0] if line[0][0] == '%' else {}) for line in input1}
-for m in modules.keys():
-    if isinstance(modules[m][1], dict):
-        for k2 in modules.keys():
-            if m != k2 and m[1:] in modules[k2][0]:
-                modules[m][1][k2] = 0
-for i in range(1000):
-    low_pulse += 1
-
-    send_pulses('broadcaster', 'low', None)
-    while len(execute) > 0:
-        k, p, prev = execute.popleft()
-        send_pulses(k, p, prev)
-# print(low_pulse, high_pulse)
-print(low_pulse * high_pulse)
-for cj in list(part2.keys()):
+def run(times, prt=True):
+    global low_pulse, high_pulse
+    low_pulse, high_pulse = 0, 0
     cycle = 0
     modules = {line[0].strip(): (line[1].split(','), [0] if line[0][0] == '%' else {}) for line in input1}
-    for _ in range(4000):
+    for m in modules.keys():
+        if isinstance(modules[m][1], dict):
+            for k2 in modules.keys():
+                if m != k2 and m[1:] in modules[k2][0]:
+                    modules[m][1][k2] = 0
+    for i in range(times):
         low_pulse += 1
         cycle += 1
-        send_pulses('broadcaster', 'low', None)
+        send_pulses('broadcaster', 'low', None, modules, cycle)
         while len(execute) > 0:
             k, p, prev = execute.popleft()
-            send_pulses(k, p, prev)
-        if sum(part2.values())/4 == list(part2.values())[0] and cycle > 5:
-            break
-# just part 1, part two should be able to be solved with lcm, but for some reason I am not able to currently find those cycles with this code
+            send_pulses(k, p, prev, modules, cycle)
+    if prt:
+        print(low_pulse, high_pulse)
+        print(low_pulse * high_pulse)
+    else:
+        print(lcm(*list(part2.values())))
+
+
+run(1000)
+run(4000, False)
